963fd3dc7ec14e24745bc74317ab85d95e1afb3c
unknown <spenurkar@gmail.com>
2014-04-23 14:23:33 +0530

    Added WifiApControl class manages wifi hotspot related functionalities

--- src/com/doctor/battery/activities/TestActivity.java
+++ src/com/doctor/battery/activities/TestActivity.java
@@ -1,11 +1,15 @@
 package com.doctor.battery.activities;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.doctor.battery.R;
 import com.doctor.battery.utils.MemoryUtils;
+import com.doctor.battery.utils.WifiApControl;
 
 public class TestActivity extends BatteryUpdateReceiverActivity {
 
@@ -20,6 +24,37 @@ public class TestActivity extends BatteryUpdateReceiverActivity {
 		imgView = (ImageView) findViewById(R.id.imageView);
 
 		initBatteryUpdateReceiver();
+
+		/**
+		 * Example for Wifi Hotspot settings
+		 */
+		// Check if Wifi tathering is supported or not
+		boolean isSupported = WifiApControl.isApSupported();
+		textView.append("\nWIFI TATHERING IS SUPPORTED." + isSupported);
+
+		// Initialize WifiApControl
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		WifiApControl wifiApControl = WifiApControl.getApControl(wifiManager);
+
+		WifiConfiguration config = wifiApControl.getWifiApConfiguration();
+		textView.append("\n" + config.SSID);
+
+		// This is how you got wifiap state
+		int state = wifiApControl.getWifiApState();
+		textView.append("\nWifi AP enabled state : " + state);
+
+		// Check wifiap state
+		if (wifiApControl.isWifiApEnabled()) {
+			textView.append("\nDisabling wifi ap...");
+			config = wifiApControl.getWifiApConfiguration();
+			config.SSID = "TetherFallback";
+			// Disable wifiap state
+			wifiApControl.setWifiApEnabled(config, false);
+		}
+
+		// This is how you got wifiap state
+		state = wifiApControl.getWifiApState();
+		textView.append("\nWifi AP enabled state now : " + state);
 	}
 
 	@Override
@@ -38,9 +73,9 @@ public class TestActivity extends BatteryUpdateReceiverActivity {
 				+ MemoryUtils.getAvailableInternalMemorySize() + "/"
 				+ MemoryUtils.getTotalInternalMemorySize());
 
-		textView.append("\n\nDevice RAM : "+MemoryUtils.getDeviceRam());
-		
-		//MemoryUtils.testMemoryInfo(this);
+		textView.append("\n\nDevice RAM : " + MemoryUtils.getDeviceRam());
+
+		// MemoryUtils.testMemoryInfo(this);
 	}
 
 }
